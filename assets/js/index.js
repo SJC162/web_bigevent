29140335aa092bbb721de0c42a502f628eb7cdc2
sjc <sjc18234580595@163.com>
2022-03-21 11:30:18 +0800

    完成了主页功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,59 +1,56 @@
-$(function () {
+$(function() {
     getUserInfo()
-    // 退出操作
     var layer = layui.layer
-    $('#btnLogout').on('click', function () {
-        layer.confirm('是否退出', { icon: 3, title: '提示' }, function (index) {
-            // 清除本地存储中的token
+    $('#btnLogout').on('click', function() {
+        //提示用户是否确认退出
+        layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            //1、 清除本在存储的token
             localStorage.removeItem('token')
-            // 重新跳转到登录页面
-            location.href = '/login.html'
-
-
-            layer.close(index)
-        })
+                //2、 重新跳转到登录页面
+            location.href = '../../login.html'
+                //关闭confirm询问框
+            layer.close(index);
+        });
     })
 })
-// 获取用户基本信息
+
+//获取用户基本信息
 function getUserInfo() {
     $.ajax({
         method: 'GET',
         url: '/my/userinfo',
-        // headers请求头配置对象
         // headers: {
-        //     Authorization: localStorage.getItem('token') || ''
+        //     Authorization: localStorage.getItem('token') || " "
         // },
-        success: function (res) {
+        success: function(res) {
             if (res.status !== 0) {
-                return layui.layer.msg('获取用户信息失败')
+                return layer.msg('获取用户信息失败！')
             }
+            //调用renderAvatar渲染用户头像
             renderAvatar(res.data)
         },
-        // complete: function (res) {
-        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败!') {
-        //         localStorage.removeItem('token')
-        //         location.href = '/login.html'
-        //     }
-        // }
+        /* complete: function(res) {
+            // console.log('执行了回调');
+            console.log(res);
+            //在complete函数可以通过responseJSON拿到服务器响应回来的数据
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                localStorage.removeItem('token')
+                location.href = '../../login.html'
+            }
+        } */
     })
 }
 
-// 渲染用户头像
 function renderAvatar(user) {
-    // 1.获取用户名称
     var name = user.nickname || user.username
-    // 2.显示用户名称
-    $('#weclome').html('欢迎&nbsp&nbsp   ' + name)
-    // 3.按需渲染用户头像
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
     if (user.user_pic !== null) {
-        // 3.1渲染图片头像
         $('.layui-nav-img').attr('src', user.user_pic).show()
-        $('.text-avatar').hide()
+        $('.text-avator').hide()
     } else {
-        // 3.2渲染文本头像
         $('.layui-nav-img').hide()
         var first = name[0].toUpperCase()
-        $('.text-avatar').html(first).show()
+        $('.text-avator').html(first).show()
     }
 }
-
